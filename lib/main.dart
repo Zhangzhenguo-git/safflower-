babc12ab946c7871d392b33eff6401e89195d3a6
Zhangzhenguo-git <826189539@qq.com>
2020-08-19 22:17:30 +0800

    '创建项目添加底部导航栏:添加导航栏点击事件onTap'

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import './navigator/tab_navigator.dart';
+main() {
+  runApp(safflower());
+}
+
+/**
+ * 项目主入口
+ */
+class safflower extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'hello word',
+      home:TabNavigator()
+    );
+  }
+}
+
---
+++ lib/navigator/tab_navigator.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+//底部导航页------
+import '../page/home_page.dart';
+import '../page/search_page.dart';
+import '../page/travel_page.dart';
+import '../page/my_page.dart';
+
+class TabNavigator extends StatefulWidget {
+  @override
+  _TabNavigatorState createState() => _TabNavigatorState();
+}
+
+class _TabNavigatorState extends State<TabNavigator> {
+//  导航页默认显示第个导航页
+  PageController _controller = PageController(initialPage: 0);
+
+//  设置默认颜色
+  final Color  _defaultColor = Colors.grey;
+//  设置选中状态下颜色
+  final Color  _activeColor = Colors.blue;
+
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:Text('小红花'),
+      ),
+      body: PageView(
+        controller: _controller,
+        children: [HomePage(), SearchPage(), TravelPage(), MyPage()],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            _controller.jumpToPage(_currentIndex);
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.home,
+                  color: _defaultColor,
+                ),
+                activeIcon: Icon(
+                  Icons.home,
+                  color: _activeColor,
+                ),
+                title: Text(
+                  '首页',
+                  style: TextStyle(
+                      color: _currentIndex != 1 ? _defaultColor : _activeColor),
+                )),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.search,
+                  color: _defaultColor,
+                ),
+                activeIcon: Icon(
+                  Icons.search,
+                  color: _activeColor,
+                ),
+                title: Text(
+                  '搜索',
+                  style: TextStyle(
+                      color: _currentIndex != 2 ? _defaultColor : _activeColor),
+                )),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.camera_alt,
+                  color: _defaultColor,
+                ),
+                activeIcon: Icon(
+                  Icons.camera_alt,
+                  color: _activeColor,
+                ),
+                title: Text(
+                  '旅拍',
+                  style: TextStyle(
+                      color: _currentIndex != 3 ? _defaultColor : _activeColor),
+                )),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.account_circle,
+                  color: _defaultColor,
+                ),
+                activeIcon: Icon(
+                  Icons.account_circle,
+                  color: _activeColor,
+                ),
+                title: Text(
+                  '我的',
+                  style: TextStyle(
+                      color: _currentIndex != 4 ? _defaultColor : _activeColor),
+                )),
+          ]),
+    );
+  }
+}
